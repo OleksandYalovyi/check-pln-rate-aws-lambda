0390987888022ec26d72ad7b453399fb349037b4
Alex Yalovyi <yalovyialex@gmail.com>
2024-09-10 20:34:26 +0200

    add console.logs

--- index.mjs
+++ index.mjs
@@ -10,20 +10,25 @@ const sendMsgToBot = async (msg, chatId) => {
 };
 
 export const handler = async () => {
+  console.log(1);
   const res = await axios.get(
     "https://rest.coinapi.io/v1/exchangerate/USD/PLN?apikey=044008B4-27A0-437E-8F26-842F42A4CFE9"
   );
+  console.log(2);
 
   const rate = Number(res.data.rate).toFixed(2);
   console.log("res", res);
   console.log("rate", rate);
 
+  console.log(3);
   if (rate > process.env.UPPER_THRESHOLD) {
     await sendMsgToBot(
       `Coin api PLN rate = ${rate}!!! It's time to sell USD to PLN!!!`,
       process.env.CHAT_ID
     );
   }
+
+  console.log(4);
 
   if (rate < process.env.LOWER_THRESHOLD) {
     await sendMsgToBot(
